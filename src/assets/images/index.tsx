143996cd3509ad18926c8841b3fda4b3f6556550
duynt2499 <118056890+duynt99@users.noreply.github.com>
2023-03-02 18:44:04 +0700

    Update home background

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -2,5 +2,6 @@ import ChartImg from "./chart.png";
 import HomeImg from "./home.png";
 import LogoWithText from "./AlphaQuest-Logo.png";
 import ETHIcon from "./eth.png";
+import HomeBgImg from "./home-bg.png";
 
-export { ChartImg, HomeImg, LogoWithText, ETHIcon };
+export { ChartImg, HomeImg, LogoWithText, ETHIcon, HomeBgImg };
