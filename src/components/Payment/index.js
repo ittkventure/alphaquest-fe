ca12ce2cc63a9631d08c7f7de7288aa3f4aa02db
duynt2499 <118056890+duynt99@users.noreply.github.com>
2023-04-11 21:17:10 +0700

    Update index.js

--- src/components/Payment/index.js
+++ src/components/Payment/index.js
@@ -6,13 +6,13 @@ export default function PaddleLoader() {
     <Script
       src="https://cdn.paddle.com/paddle/paddle.js"
       onLoad={() => {
-        if (process.env.NEXT_PUBLIC_PADDLE_SANDBOX) {
-          Paddle.Environment.set(
-            process.env.NEXT_PUBLIC_PADDLE_SANDBOX ?? "sandbox"
-          );
-        }
+        // if (process.env.NEXT_PUBLIC_PADDLE_SANDBOX) {
+        //   Paddle.Environment.set(
+        //     process.env.NEXT_PUBLIC_PADDLE_SANDBOX ?? "sandbox"
+        //   );
+        // }
         Paddle.Setup({
-          vendor: Number(process.env.NEXT_PUBLIC_PADDLE_VENDOR_ID ?? 11441),
+          vendor: Number(167184),
         });
       }}
     />
