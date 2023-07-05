526ca1b4ed0557bc5dad32654f79b6b5fc615837
Thanh Duy <118056890+duynt99@users.noreply.github.com>
2023-07-05 21:34:22 +0700

    update full detail page

--- src/components/AlphaHunter/AlphaCard.tsx
+++ src/components/AlphaHunter/AlphaCard.tsx
@@ -20,7 +20,7 @@ interface IAlphaCard {
 const AlphaCard: FC<IAlphaCard> = ({ items, label, isLoading }) => {
   const renderDesktop = () => {
     return (
-      <div className="w-full bg-dark-800 p-6 max-xl:hidden flex flex-col justify-between">
+      <div className="w-full bg-dark-800 p-6 max-xl:hidden  flex flex-col justify-between">
         <div>
           <p className="text-[18px] leading-5">{label}</p>
 
