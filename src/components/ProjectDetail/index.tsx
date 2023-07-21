613c8955601f094b3485782ff409683150d5c660
Thanh Duy <118056890+duynt99@users.noreply.github.com>
2023-07-21 10:38:18 +0700

    update position of share btn

--- src/components/ProjectDetail/index.tsx
+++ src/components/ProjectDetail/index.tsx
@@ -25,7 +25,6 @@ import {
   TwitterIcon as TwitterIconShare,
   TwitterShareButton,
 } from "react-share";
-import Tippy from "@tippyjs/react";
 
 interface IProjectDetail {
   userId?: string;
@@ -323,75 +322,80 @@ const ProjectDetail: FC<IProjectDetail> = ({
                         </div>
                         <div>{_renderHeartButton()}</div>
                       </div>
-                      <div className="flex max-lg:flex-col max-lg:justify-between  justify-end items-center mt-2">
-                        <p>Share: </p>
-                        <div className="flex items-center">
-                          <TelegramShareButton url={shareUrl} className="ml-1">
-                            <TelegramIcon size={22} round />
-                          </TelegramShareButton>
-                          <TwitterShareButton url={shareUrl} className="ml-1">
-                            <TwitterIconShare size={22} round />
-                          </TwitterShareButton>
-                          <FacebookShareButton url={shareUrl} className="ml-1">
-                            <FacebookIcon size={22} round />
-                          </FacebookShareButton>
-
-                          <div className="ml-1">
-                            <button
-                              onClick={() => {
-                                navigator.clipboard.writeText(shareUrl);
-                                toast.success("Copied to clipboard");
-                              }}
-                            >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="#6B7280"
-                                className="w-6 h-6 mt-[6px] hover:stroke-slate-200"
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"
-                                />
-                              </svg>
-                            </button>
-                          </div>
-                        </div>
-                      </div>
                     </div>
                   </div>
                   <p className="text-sm mt-3">
                     {twitterDetail.data?.description}
                   </p>
 
-                  <div className="flex mt-3">
-                    {twitterDetail.data && twitterDetail.data?.categories
-                      ? twitterDetail.data?.categories?.map((value, index) => (
-                          <a
-                            href={`/projects?category=${value?.code}`}
+                  <div className="flex justify-between mt-3 items-center">
+                    <div className="flex">
+                      {twitterDetail.data && twitterDetail.data?.categories
+                        ? twitterDetail.data?.categories?.map(
+                            (value, index) => (
+                              <a
+                                href={`/projects?category=${value?.code}`}
+                                onClick={() => {
+                                  mixpanelTrack(
+                                    event_name_enum.on_filter_category,
+                                    {
+                                      url: router.pathname,
+                                      name: value?.name,
+                                      code: value?.code,
+                                    }
+                                  );
+                                }}
+                              >
+                                <p
+                                  key={index.toString()}
+                                  className="text-sm border px-2 mr-2"
+                                >
+                                  {value.name}
+                                </p>
+                              </a>
+                            )
+                          )
+                        : null}
+                    </div>
+
+                    <div className="flex max-lg:flex-col max-lg:justify-between  justify-end items-center">
+                      <p>Share: </p>
+                      <div className="flex items-center">
+                        <TelegramShareButton url={shareUrl} className="ml-1">
+                          <TelegramIcon size={22} round />
+                        </TelegramShareButton>
+                        <TwitterShareButton url={shareUrl} className="ml-1">
+                          <TwitterIconShare size={22} round />
+                        </TwitterShareButton>
+                        <FacebookShareButton url={shareUrl} className="ml-1">
+                          <FacebookIcon size={22} round />
+                        </FacebookShareButton>
+
+                        <div className="ml-1">
+                          <button
                             onClick={() => {
-                              mixpanelTrack(
-                                event_name_enum.on_filter_category,
-                                {
-                                  url: router.pathname,
-                                  name: value?.name,
-                                  code: value?.code,
-                                }
-                              );
+                              navigator.clipboard.writeText(shareUrl);
+                              toast.success("Copied to clipboard");
                             }}
                           >
-                            <p
-                              key={index.toString()}
-                              className="text-sm border px-2 mr-2"
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth={1.5}
+                              stroke="#6B7280"
+                              className="w-6 h-6 mt-[6px] hover:stroke-slate-200"
                             >
-                              {value.name}
-                            </p>
-                          </a>
-                        ))
-                      : null}
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"
+                              />
+                            </svg>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
                     {/* {_renderNewTag()} */}
                   </div>
                 </div>
