142b1a8ebf84ffc608893986c9fcea218eabd261
Alex Nguyen <118056890+duynt99@users.noreply.github.com>
2023-11-27 17:50:26 +0700

    fix: fix spacing of circle

--- src/components/TopAlphaHunterByDiscoveries/Table/Rows/ProjectsFollowed/index.tsx
+++ src/components/TopAlphaHunterByDiscoveries/Table/Rows/ProjectsFollowed/index.tsx
@@ -17,22 +17,18 @@ const ProjectsFollowedRow: FC<IProjectsFollowedRowProps> = ({
     "projectsFollowedLastXDays.length"
   );
 
-  const exactLength = projectsFollowedLastXDays.filter((item) => {
-    return item.projectsCount !== 0;
-  }).length;
-
   return (
     <div className="relative flex items-center">
       <div className="absolute w-full pr-10">
         <div className="w-full border-2 border-dashed border-[#2D354D] " />
       </div>
       <div className="flex gap-3 justify-center items-center w-full pr-10">
-        {exactLength % 2 !== 0 && exactLength < 10 && (
-          <div className="w-8 h-8 min-w-[32px] min-h-[32px] rounded-full" />
-        )}
         {projectsFollowedLastXDays.map((item) => {
           const id = uuidv4();
-          if (item.projectsCount === 0) return null;
+          if (item.projectsCount === 0)
+            return (
+              <div className="w-8 h-8 min-w-[32px] min-h-[32px] rounded-full" />
+            );
           return (
             <>
               <Popup
@@ -56,22 +52,28 @@ const ProjectsFollowedRow: FC<IProjectsFollowedRowProps> = ({
                 keepTooltipInside=".tooltipBoundary"
               >
                 <div className="bg-[#282E44] pt-4 pb-2 max-h-[377px] overflow-y-scroll overflow-x-hidden">
-                  {item.projects.map((project, index) => {
+                  {item.projects.map((project) => {
                     return (
-                      <div className="flex items-center gap-2 mb-3 bg-[#282E44] px-6">
-                        <p>Followed</p>
-                        <img
-                          src={project.profileImageUrl}
-                          alt=""
-                          className="w-8 h-8 min-w-[32px] min-h-[32px] bg-white rounded-full"
-                        />
-                        <p className="font-workSansMedium">
-                          {project.name} on{" "}
-                          {moment(project.followingTime)
-                            .utc()
-                            .format("MM/DD/YYYY")}
-                        </p>
-                      </div>
+                      <a
+                        key={project.username}
+                        href={`/project/${project.username}`}
+                        target="_blank"
+                      >
+                        <div className="flex items-center gap-2 mb-3 bg-[#282E44] px-6">
+                          <p>Followed</p>
+                          <img
+                            src={project.profileImageUrl}
+                            alt=""
+                            className="w-8 h-8 min-w-[32px] min-h-[32px] bg-white rounded-full"
+                          />
+                          <p className="font-workSansMedium">
+                            {project.name} on{" "}
+                            {moment(project.followingTime)
+                              .utc()
+                              .format("MM/DD/YYYY")}
+                          </p>
+                        </div>
+                      </a>
                     );
                   })}
                 </div>
