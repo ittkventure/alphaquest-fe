8ca44afdb8ad7deb7a97a8b1d8bde35264f9286c
Truong Phuc Hung <hungtp1996nd@gmail.com>
2023-12-08 15:25:39 +0700

    integrate quick search api, remove filter into client

--- src/components/App/QuickSearch.tsx
+++ src/components/App/QuickSearch.tsx
@@ -1,54 +1,63 @@
-import { useState, useEffect } from "react";
+import { useMemo } from "react";
 import Image from "next/image";
 import { AlphaHunterIcon } from "@/assets/icons";
-import { filterDataSearch, getSearchDataByType } from "@/utils/quickSearch";
-import { SEARCH_TYPE, SearchItem } from "@/api-client/types/QuickSearch";
-import localforage from "localforage";
+import { SearchItem } from "@/api-client/types/QuickSearch";
+import { useQuery } from "react-query";
+import { fetchQuickSearch } from "@/api-client/quick-search";
+import { useRouter } from "next/router";
 
 type QuickSearchProps = {
   searchString: string;
+  closeQuickSearch: () => void;
+  resetSearch: () => void;
 };
 
-export default function QuickSearch({ searchString }: QuickSearchProps) {
-  const [projects, setProjects] = useState<SearchItem[]>([]);
-  const [alphahunters, setAlphaHunters] = useState<SearchItem[]>([]);
-  const [narratives, setNarratives] = useState<SearchItem[]>([]);
+export default function QuickSearch({
+  searchString,
+  closeQuickSearch,
+  resetSearch,
+}: QuickSearchProps) {
+  const router = useRouter();
+  const { data: quickSearchData } = useQuery(
+    ["getQuickSearch", searchString, 50],
+    () => fetchQuickSearch(searchString, 50)
+  );
 
-  localforage.getItem("narratives", (err, value: string | null) => {
-    if (!value) return;
-    const narratives = filterDataSearch(
-      JSON.parse(value),
-      ["displayName"],
-      searchString
-    );
-    setNarratives(narratives);
-  });
-  localforage.getItem("projects", (err, value: string | null) => {
-    if (!value) return;
-    const projects = filterDataSearch(
-      JSON.parse(value),
-      ["displayName"],
-      searchString
-    );
-    setProjects(projects);
-  });
-  localforage.getItem("alphahunters", (err, value: string | null) => {
-    if (!value) return;
-    const alphahunters = filterDataSearch(
-      JSON.parse(value),
-      ["displayName"],
-      searchString
-    );
-    setAlphaHunters(alphahunters);
-  });
+  const projects = useMemo(() => {
+    if (!quickSearchData) return;
+    return quickSearchData?.project;
+  }, [quickSearchData]);
+
+  const narratives = useMemo(() => {
+    if (!quickSearchData) return;
+    return quickSearchData?.narratives;
+  }, [quickSearchData]);
+
+  const alphahunters = useMemo(() => {
+    if (!quickSearchData) return;
+    return quickSearchData["alpha-hunter"];
+  }, [quickSearchData]);
+
   return (
-    <div className="absolute right-0 top-12 p-4 bg-[#292C35] w-[480px] h-[70vh] z-10 flex flex-col gap-4 text-white overflow-y-auto divide-y divide-[#52525B]">
+    <div className="absolute right-0 top-12 p-4 bg-[#292C35] w-[480px] h-[70vh] z-20 flex flex-col gap-4 text-white overflow-y-auto divide-y divide-[#52525B]">
       {searchString && (
         <>
           <div className="flex flex-col gap-4">
             <span className="uppercase text-xs font-semibold">Projects</span>
             {projects?.map((project: SearchItem) => (
-              <div className="flex justify-between items-center text-sm font-light">
+              <div
+                className="flex justify-between items-center text-sm font-light cursor-pointer hover:bg-secondary-300"
+                key={project?.key}
+                onClick={() => {
+                  router.push(
+                    project?.metadata
+                      ? `/project/${JSON.parse(project?.metadata)?.username}`
+                      : `/projects`
+                  );
+                  closeQuickSearch();
+                  resetSearch();
+                }}
+              >
                 <div className="flex items-center gap-3">
                   <Image
                     src={
@@ -63,14 +72,28 @@ export default function QuickSearch({ searchString }: QuickSearchProps) {
                   />
                   <span>{project?.displayName}</span>
                 </div>
-                <span>{`#${project?.sortOrder}`}</span>
+                <span>
+                  {project?.sortOrder ? `#${project?.sortOrder}` : undefined}
+                </span>
               </div>
             ))}
           </div>
           <div className="flex flex-col gap-4 pt-4">
             <span className="uppercase text-xs font-semibold">Narratives</span>
             {narratives?.map((narrative: SearchItem) => (
-              <div className="flex justify-between items-center text-sm font-light">
+              <div
+                className="flex justify-between items-center text-sm font-light cursor-pointer hover:bg-secondary-300"
+                key={narrative?.key}
+                onClick={() => {
+                  router.push(
+                    narrative?.key
+                      ? `/narratives/${narrative?.key}`
+                      : `/narratives`
+                  );
+                  closeQuickSearch();
+                  resetSearch();
+                }}
+              >
                 <div className="flex items-center gap-3">
                   <Image
                     src={AlphaHunterIcon}
@@ -81,7 +104,11 @@ export default function QuickSearch({ searchString }: QuickSearchProps) {
                   />
                   <span>{narrative?.displayName}</span>
                 </div>
-                <span>{`#${narrative?.sortOrder}`}</span>
+                <span>
+                  {narrative?.sortOrder
+                    ? `#${narrative?.sortOrder}`
+                    : undefined}
+                </span>
               </div>
             ))}
           </div>
@@ -90,7 +117,19 @@ export default function QuickSearch({ searchString }: QuickSearchProps) {
               Alpha Hunters
             </span>
             {alphahunters?.map((alpha: SearchItem) => (
-              <div className="flex justify-between items-center text-sm">
+              <div
+                className="flex justify-between items-center text-sm cursor-pointer hover:bg-secondary-300"
+                key={alpha?.key}
+                onClick={() => {
+                  router.push(
+                    alpha?.metadata
+                      ? `/alpha-hunter/${JSON.parse(alpha?.metadata)?.username}`
+                      : `/alpha-hunters`
+                  );
+                  closeQuickSearch();
+                  resetSearch();
+                }}
+              >
                 <div className="flex items-center gap-3">
                   <Image
                     src={
