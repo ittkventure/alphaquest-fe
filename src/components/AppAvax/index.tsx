d2ea92fed35c45803711f44dcf7c3d9e0ca10b45
Alex Nguyen <118056890+duynt99@users.noreply.github.com>
2023-12-16 19:07:44 +0700

    update default 10 page for not login

--- src/components/AppAvax/index.tsx
+++ src/components/AppAvax/index.tsx
@@ -200,6 +200,7 @@ const AppContent: FC<AppContentTypes> = ({
   };
 
   const fetchDataLoadMore = async () => {
+    if (!getAccessToken()) return;
     try {
       setIsLoadingMore(true);
       setErrorMsg("");
